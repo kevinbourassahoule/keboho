4a4a8f6fa11776ff616a65f623b23dad67ecf810
lordkevinthebold <kevinbourassahoule@gmail.com>
2019-04-27 21:51:16 -0400

    init from template

---
+++ Keboho/Keboho.ServiceModel/Hello.cs
@@ -0,0 +1,18 @@
+using ServiceStack;
+
+namespace Keboho.ServiceModel
+{
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+}
